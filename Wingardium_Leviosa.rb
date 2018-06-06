d64323c4d3ca834587d2454b428f1bc3a9bf2b0c
apw601 <apw601@gmail.com>
2018-06-06 18:03:45 +0000

    automatically backed up by learn

--- Wingardium_Leviosa.rb
+++ Wingardium_Leviosa.rb
@@ -1,12 +1,10 @@
 def until_loop
   force = 6
   max_force = 10
-  until max_force = 10 do
+  until max_force == 10 do
   puts "Wingardium Leviosa"
   force + 1
   end
 end
 
 until_loop
-
-def while_loop
